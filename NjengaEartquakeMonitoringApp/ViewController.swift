d740adf26c7e4ebc0ef9a2231c3b284a5eb42687
Kyym <moriskashing74.com>
2024-06-19 14:36:06 +0300

    ok

--- NjengaEartquakeMonitoringApp/ViewController.swift
+++ NjengaEartquakeMonitoringApp/ViewController.swift
@@ -4,16 +4,47 @@
 //
 //  Created by Gracie on 18/06/2024.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    let imageView = UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imageView.frame = view.bounds
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "launch image")
+        imageView.alpha = 0
+        view.addSubview(imageView)
+
+      
+        UIView.animate(withDuration: 1.0, animations: {
+            self.imageView.alpha = 1
+        }) { (finished) in
+           
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.fadeOutImage()
+            }
+        }
     }
 
+    func fadeOutImage() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.imageView.alpha = 0
+        }) { (finished) in
+            
+            self.imageView.removeFromSuperview()
+        
+            self.transitionToMainInterface()
+        }
+    }
 
+    func transitionToMainInterface() {
+
+        view.backgroundColor = .white
+    }
 }
+
 
