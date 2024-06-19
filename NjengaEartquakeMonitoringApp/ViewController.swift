d978da08e9f14a45c19105266812b99401070330
Kyym <moriskashing74.com>
2024-06-19 16:37:12 +0300

    okay

--- NjengaEartquakeMonitoringApp/ViewController.swift
+++ NjengaEartquakeMonitoringApp/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AppDelegate.swift
 //  NjengaEartquakeMonitoringApp
 //
 //  Created by Gracie on 18/06/2024.
@@ -7,40 +7,44 @@
 
 import UIKit
 
-class ViewsController: UIViewController {
+class ViewController: UIViewController {
 
     let imageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50) // Start with a small size
-        imageView.center = view.center
+        imageView.frame = view.bounds
         imageView.contentMode = .scaleAspectFill
-        imageView.image = UIImage(named: "launch image") // Replace with your image name
-        imageView.alpha = 0 // Start with image view hidden
+        imageView.image = UIImage(named: "launch image")
+        imageView.alpha = 0
         view.addSubview(imageView)
 
-        // Animate the image view scaling and fading in
         UIView.animate(withDuration: 1.0, animations: {
-            self.imageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0) // Scale up to 2 times
-            self.imageView.alpha = 1 // Fade in
+            self.imageView.alpha = 1
         }) { (finished) in
-            // Delay for 4 seconds before starting the explosion animation
-            DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
-                self.explodeImage()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                self.fadeOutImage()
             }
         }
     }
 
-    func explodeImage() {
-        // Scale up and fade out animation
-        UIView.animate(withDuration: 0.5, animations: {
-            self.imageView.transform = CGAffineTransform(scaleX: 5.0, y: 5.0) // Scale up to 5 times
-            self.imageView.alpha = 0 // Fade out
+    func fadeOutImage() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.imageView.alpha = 0
         }) { (finished) in
-            // Remove imageView from superview or reset as needed
             self.imageView.removeFromSuperview()
+            self.performSegue(withIdentifier: "segueToNextScreen", sender: self) // Update segue identifier
         }
     }
-}
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToNextScreen" {
+            // Optionally, you can pass data to the destination view controller here
+            // For example:
+            // if let destinationVC = segue.destination as? NextViewController {
+            //     destinationVC.someProperty = someValue
+            }
+        }
+    }
+
